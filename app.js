3823eeacee8b24b7412e647c558a12de987632ea
James Schwab <jamesrobschwab@gmail.com>
2023-10-10 20:10:33 +1100

    Push changes to test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ let playerData = {};
 function fetchPlayerData() {
     // Generate fetch promises for all teams
     const fetchPromises = teams.map(team => 
-        fetch(`${team.toLowerCase().replace(/\s+/g, '')}.json`).then(response => {
+        fetch(`data-clubplayers/${team.toLowerCase().replace(/\s+/g, '')}.json`).then(response => {
             // If fetching data for a team fails, log the error
             if (!response.ok) {
                 console.error(`Failed to fetch data for team: ${team}`);
@@ -60,26 +60,58 @@ function findPlayers() {
     const matchingPlayers = team1Players.filter(player1 => 
         team2Players.some(player2 => player1.id === player2.id)
     ).map(player1 => {
-        const player2Data = team2Players.find(player2 => player1.id === player2.id).data;
+        // Convert "DOB" string to a Date object
+        const convertedDOB = new Date(player1.data["DOB"].split("/")[2], player1.data["DOB"].split("/")[1] - 1, player1.data["DOB"].split("/")[0]);
+
+        // Log the conversion result to ensure it's correct
+        console.log(player1.data["Player"], convertedDOB); 
+
         return {
-            name: player1.data["Player"],
+            name: player1.data["Player"].split(', ').reverse().join(' '),
             team1Seasons: player1.data["Seasons"],
-            team2Seasons: player2Data["Seasons"],
-            dob: new Date(player1.data["DOB"].split("/")[2], player1.data["DOB"].split("/")[1] - 1, player1.data["DOB"].split("/")[0])  // Convert "DOB" string to a Date object
-        };
+            team2Seasons: team2Players.find(player2 => player1.id === player2.id).data["Seasons"],
+            dob: convertedDOB
+        };        
     });
 
     // Sort the matching players by their DOB (most recently born to least recently)
     matchingPlayers.sort((a, b) => b.dob - a.dob);
+
+    // Log the sorted players to ensure the sorting worked
+    console.log(matchingPlayers);
 
     const playerList = document.getElementById('playerList');
     playerList.innerHTML = '';
 
     matchingPlayers.forEach(player => {
         const li = document.createElement('li');
-        li.textContent = `${player.name} - ${team1}: ${player.team1Seasons}, ${team2}: ${player.team2Seasons}`;
+        
+        const playerName = document.createElement('div');
+        playerName.textContent = player.name;
+        li.appendChild(playerName);
+        
+        const team1Seasons = document.createElement('div');
+        team1Seasons.textContent = `${team1}: ${player.team1Seasons}`;
+        li.appendChild(team1Seasons);
+        
+        const team2Seasons = document.createElement('div');
+        team2Seasons.textContent = `${team2}: ${player.team2Seasons}`;
+        li.appendChild(team2Seasons);
+        
         playerList.appendChild(li);
     });
+    
+}
+
+function updateTeamImage(teamSelectId) {
+    const selectedTeam = document.getElementById(teamSelectId).value;
+    
+    // Assuming the images are named exactly as the team names but in lowercase and without spaces, and they're in an "images" directory
+    const imageUrl = `images/${selectedTeam.toLowerCase().replace(/\s+/g, '')}.png`;
+
+    const imageElement = document.getElementById(teamSelectId + 'Image');
+    imageElement.src = imageUrl;
+    imageElement.style.display = "block"; // Make the image visible
 }
 
 
